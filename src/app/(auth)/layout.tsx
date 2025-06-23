9f4a373fe01a2371d945ec5749b15773d3f9f559
donghwan <kdhe9797@naver.com>
2025-06-23 18:33:15 +0900

    refactor : 로그인 회원가입 폼 수정

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,7 +1,7 @@
 // layout.tsx
 export default function Layout({children}: {children: React.ReactNode}) {
   return (
-    <div className="flex flex-col min-h-screen w-full">
+    <div className="flex flex-col w-full min-h-screen">
       <div className="flex flex-1">{children}</div>
     </div>
   )
